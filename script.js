7da44e68c2ad34a3c3d5c35bead19c93ccd58c81
sakincse21 <sakin.cse21@gmail.com>
2024-06-29 05:27:14 +0600

    fixing words ig

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //const url='https://gist.githubusercontent.com/ijmacdowell/8325491/raw/b39de3a6ba03205380caf5d58e0cae8a869ac36d/nouns.js';
-const url='words.json';
+const url='https://raw.githubusercontent.com/sakincse21/hangman-game/main/words.json';
 const getWord= async () => {
     let response = await fetch(url);
     console.log(response);
